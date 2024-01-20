e95ea534cdd905640f61e558be896ad1e2170135
joeng03 <yinjoeng2003@gmail.com>
2024-01-20 23:25:40 +0800

    style: restyle levels page

--- src/app/level-selector/page.tsx
+++ src/app/level-selector/page.tsx
@@ -1,20 +1,16 @@
-import {levels} from "@/levels";
+import { levels } from "@/levels";
 import LevelPreview from "./level-preview";
 
 export default async function LevelSelector() {
   return (
     <main className="flex min-h-screen flex-col items-start justify-center bg-[url('/bg.jpeg')] bg-contain">
       <div className="flex items-start justify-start">
-        <div className="overflow-x-scroll hide-scroll-bar p-40">
-          <div className="h-[1px] bg-white w-[3000px] px-20"></div>
-          <div className="flex justify-start">
-          {
-            levels.map((level, index) => {
-              return (
-                <LevelPreview key={index} levelNo={level.levelNo} />
-              );
-            })
-          }
+        <div className="hide-scroll-bar overflow-x-scroll p-40">
+          <div className="h-[1px] bg-white px-20"></div>
+          <div className="flex flex-col justify-start gap-8">
+            {levels.map((level, index) => {
+              return <LevelPreview key={index} levelNo={level.levelNo} />;
+            })}
           </div>
         </div>
       </div>
