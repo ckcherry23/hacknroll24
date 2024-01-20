251836e0753827c25ef2c0210c0b103759bb9e2d
joeng03 <yinjoeng2003@gmail.com>
2024-01-20 23:09:11 +0800

    feat: Add level

--- src/app/_components/stage.tsx
+++ src/app/_components/stage.tsx
@@ -1,53 +1,64 @@
-'use client'
+"use client";
 
-import React, { useEffect, useRef, useState } from 'react'
-import { RunButton, Sandpack, SandpackCodeEditor, SandpackLayout, SandpackProvider, useActiveCode, useSandpack } from '@codesandbox/sandpack-react';
-import CodePreview from './code-preview';
-import CodeEditor from './code-editor';
-import { Button } from '@/components/ui/button';
-import CodeSubmitButton from './code-submit-btn';
-import { type LevelType } from '@/lib/types';
-import ProblemStatement from './problem/problem-statement';
-
+import React, { useEffect, useRef, useState } from "react";
+import {
+  RunButton,
+  Sandpack,
+  SandpackCodeEditor,
+  SandpackLayout,
+  SandpackProvider,
+  useActiveCode,
+  useSandpack,
+} from "@codesandbox/sandpack-react";
+import CodePreview from "./code-preview";
+import CodeEditor from "./code-editor";
+import { Button } from "@/components/ui/button";
+import CodeSubmitButton from "./code-submit-btn";
+import { type LevelType } from "@/lib/types";
+import ProblemStatement from "./problem/problem-statement";
 
 type StageProps = {
   level: LevelType;
   onSubmit: (code: string) => void;
-}
+};
 
-export default function Stage({level, onSubmit}: StageProps) {
-  const [client, setClient] = useState<unknown>()
+export default function Stage({ level, onSubmit }: StageProps) {
+  const [client, setClient] = useState<unknown>();
   const [code, setCode] = useState(level.initialCode);
 
   // Used to intercept the onSubmit to get the code value
   const handleSubmit = (code: string) => {
-    setCode(code)
+    setCode(code);
     onSubmit(code);
-  }
+  };
   return (
-    <div className='w-full flex flex-col p-20 space-y-10'>
+    <div className="flex w-full flex-col space-y-10 p-20">
       <ProblemStatement level={level} />
       <SandpackProvider
-        theme={"dark"} 
-        template='react' 
+        theme={"dark"}
+        template="react"
+        customSetup={{
+          dependencies: {
+            "react-markdown": "latest",
+          },
+        }}
         options={{
           classes: {
             "sp-wrapper": "stage",
-          }
+          },
         }}
-        files={
-          {
-            "/App.js": {
-              code: code,
-            }
-          }
-        }>
-        <SandpackLayout className='flex-grow'>
+        files={{
+          "/App.js": {
+            code: code,
+          },
+        }}
+      >
+        <SandpackLayout className="flex-grow">
           <CodeEditor />
           <CodePreview setClient={setClient} />
         </SandpackLayout>
-        <CodeSubmitButton client={client} onSubmit={handleSubmit}/>
+        <CodeSubmitButton client={client} onSubmit={handleSubmit} />
       </SandpackProvider>
     </div>
-  )
+  );
 }
