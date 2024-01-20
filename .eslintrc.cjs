9dea54ebf0d4814939b8ec46f99e18a2cc8f4c22
tankh99 <khanghou99@gmail.com>
2024-01-20 23:01:59 +0800

    fix: build error

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -35,6 +35,7 @@ const config = {
     "@typescript-eslint/no-unsafe-call": "off",
     "@typescript-eslint/no-unsafe-return": "off",
     "@typescript-eslint/no-unsafe-member-access": "off",
+    "@typescript-eslint/no-explicit-any": "off"
   },
 };
 
