868e17a9db554557ce11f185186aa4916f7a4c6d
Charisma Kausar <charisma.kausar@gmail.com>
2024-01-21 02:15:01 +0800

    feat: lock not completed levels on level selector

--- src/app/level-selector/level-preview.tsx
+++ src/app/level-selector/level-preview.tsx
@@ -1,17 +1,30 @@
+'use client';
+
 import { Button } from "@/components/ui/button";
 import { LevelType } from "@/lib/types";
 import Link from "next/link";
+import { useLocalStorage } from 'usehooks-ts';
 
 type LevelPreviewProps = {
   level: LevelType;
 }
 
-export default async function LevelPreview({ level }: LevelPreviewProps) {
+export default function LevelPreview({ level }: LevelPreviewProps) {
+  const [completedLevels, setCompletedLevels] = useLocalStorage<Array<string>>(
+    'completedLevels',
+    [],
+  );
+
+  const isCompleted = completedLevels.includes(level.levelNo);
+
   return (
-    <div className="w-[800px] flex flex-col gap-8 pt-12">
+    <div>
+    <div className={`w-[800px] flex flex-col gap-8 pt-12 ${!isCompleted && "opacity-20"}`}>
       <div className="text-4xl">Level {level.levelNo} Boss</div>
       <div className="text-7xl">{level.position}</div>
-      <Link href={`level/${level.levelNo}`}><Button>Accept Offer</Button></Link>
+      <Link href={isCompleted ? `level/${level.levelNo}` : '#'}><Button disabled={!isCompleted}>Accept Offer</Button></Link>
+    </div>
+    {!isCompleted &&<div>Locked</div>}
     </div>
   );
 }
