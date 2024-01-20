9b468e1f71011d1ad427cbca8c0726996c8d31b7
sheng <chengzhisheng2001@gmail.com>
2024-01-20 15:00:58 +0800

    feat: add sample usage for openai

--- src/app/_components/ai-test.tsx
+++ src/app/_components/ai-test.tsx
@@ -1,18 +1,43 @@
 "use client";
 import { api } from "@/trpc/react";
+import { useState } from "react";
 
 export default function AiTest() {
-  const textMutation = api.openAI.hello.useMutation();
+  const textMutation = api.openAI.hello.useMutation({
+    onSuccess: (data) => {
+      const message = data.message;
+      setMessages((messages) => [...messages, message]);
+    },
+  });
+  const [input, setInput] = useState("How can I succeed as an SWE?");
+  const [messages, setMessages] = useState<string[]>([]);
+
   const onClick = async () => {
     textMutation.mutate({
-      text: "What's the tallest building in the world?",
+      message: input,
+      persona: "Angry Man",
     });
   };
 
   return (
-    <div>
-      <button onClick={onClick}>Call OpenAI</button>
-      <div>{textMutation.data?.greeting ?? ""}</div>
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-2">
+      <button
+        onClick={onClick}
+        className="rounded-xl bg-black px-4 py-2 text-white"
+      >
+        Call OpenAI
+      </button>
+      <div>Fill in your message:</div>
+      <input
+        className="w-96 border border-black px-4 py-2"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      {/* <div>{textMutation.data?.message ?? ""}</div> */}
+      {messages.map((message) => (
+        <div key={message.substring(0, 50)}>{message}</div>
+      ))}
     </div>
   );
 }
--- src/server/api/routers/openAI.ts
+++ src/server/api/routers/openAI.ts
@@ -7,24 +7,38 @@ const openai = new OpenAI({
   apiKey: env.OPENAI_API_KEY,
 });
 
-async function chatCompletion(text: string) {
-  const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: text }],
+async function chatCompletion(text: string, persona: Persona) {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: `Your persona is ${persona}, giving a code review for a junior developer. Your responses should suit the personality of the persona. Limit your responses to 100 characters.`,
+      },
+      { role: "user", content: text },
+    ],
     model: "gpt-3.5-turbo",
   });
 
-  return chatCompletion.choices[0]?.message.content;
+  return completion.choices[0]?.message.content;
 }
+
+const personaSchema = z.enum(["Software Engineer", "Elon Musk", "Angry Man"]);
+type Persona = z.infer<typeof personaSchema>;
 
 export const aiRouter = createTRPCRouter({
   hello: publicProcedure
-    .input(z.object({ text: z.string() }))
+    .input(
+      z.object({
+        message: z.string(),
+        persona: personaSchema,
+      }),
+    )
     .mutation(async ({ input }) => {
-      const { text } = input;
-      const completion = await chatCompletion(text);
+      const { message, persona } = input;
+      const completion = (await chatCompletion(message, persona)) ?? "";
 
       return {
-        greeting: `Hello ${completion ?? ""}`,
+        message: completion,
       };
     }),
 });
