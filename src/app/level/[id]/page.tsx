8ffbd9373d3b6b4c2078ee7014e4a3b8563cabd3
Charisma Kausar <charisma.kausar@gmail.com>
2024-01-20 17:57:08 +0800

    feat: add landing page skeleton

--- src/app/level/[id]/page.tsx
+++ src/app/level/[id]/page.tsx
@@ -1,7 +1,44 @@
+import { unstable_noStore as noStore } from "next/cache";
+import ChatInterface from "@/app/_components/bot/bot";
+import Stage from "@/app/_components/stage";
+import { api } from "@/trpc/server";
+import levels from "@/levels.json"
+import { type LevelType } from "@/lib/types";
+
+const level: LevelType = levels.levels[0]!;
+
+
+
 export default async function Level({ params }: { params: { id: string } }) {
+
+  noStore();
+
+  const tts = await api.tts.convert.query({
+    text: "The quick brown fox jumps over the lazy dog",
+    emotion_name: "Default",
+    person_voice: "Elon Musk",
+  });
   return (
-    <div className="text-7xl w-[800px]">
-      Level {params.id}
-    </div>
+    <main className="flex min-h-screen flex-col">
+      <div className="flex flex-row">
+        <div className="h-screen border-r-2 px-4 py-4 w-[350px] flex flex-col">
+          <ChatInterface />
+        </div>
+        <div className="flex w-full">
+
+          <Stage level={level} />
+        </div>
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-2xl text-white"></p>
+        </div>
+
+        <audio
+          src={tts.oss_url}
+          preload="auto"
+          style={{ display: "none" }}
+          autoPlay
+        ></audio>
+      </div>
+    </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Stage from "./_components/stage";
 import { api } from "@/trpc/server";
 import levels from "@/levels.json"
 import { type LevelType } from "@/lib/types";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const level: LevelType = levels.levels[0]!;
 
@@ -18,25 +20,10 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col">
-      <div className="flex flex-row">
-        <div className="h-screen border-r-2 px-4 py-4 w-[350px] flex flex-col">
-          <ChatInterface/>
-        </div>
-        <div className="flex w-full">
-
-        <Stage level={level} />
-        </div>
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-2xl text-white"></p>
-        </div>
-
-        <audio
-          src={tts.oss_url}
-          preload="auto"
-          style={{ display: "none" }}
-          autoPlay
-        ></audio>
-      </div>
+      <h1>
+        Making you tougher for the working world
+      </h1>
+      <Link href={`level-selector`}><Button>Start</Button></Link>
     </main>
   );
 }
