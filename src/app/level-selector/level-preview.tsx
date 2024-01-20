11ffa311bddebfe4ec569875846e36bf0b1c10bf
Charisma Kausar <charisma.kausar@gmail.com>
2024-01-21 02:19:45 +0800

    fix: unlock next level

--- src/app/level-selector/level-preview.tsx
+++ src/app/level-selector/level-preview.tsx
@@ -12,19 +12,20 @@ type LevelPreviewProps = {
 export default function LevelPreview({ level }: LevelPreviewProps) {
   const [completedLevels, setCompletedLevels] = useLocalStorage<Array<string>>(
     'completedLevels',
-    [],
+    ["0"],
   );
 
-  const isCompleted = completedLevels.includes(level.levelNo);
+  const isUnlocked = completedLevels.includes(level.levelNo)
+    || parseInt(level.levelNo) == Math.max(...completedLevels.map((x) => parseInt(x))) + 1;
 
   return (
     <div>
-    <div className={`w-[800px] flex flex-col gap-8 pt-12 ${!isCompleted && "opacity-20"}`}>
-      <div className="text-4xl">Level {level.levelNo} Boss</div>
-      <div className="text-7xl">{level.position}</div>
-      <Link href={isCompleted ? `level/${level.levelNo}` : '#'}><Button disabled={!isCompleted}>Accept Offer</Button></Link>
-    </div>
-    {!isCompleted &&<div>Locked</div>}
+      <div className={`w-[800px] flex flex-col gap-8 pt-12 ${!isUnlocked && "opacity-20"}`}>
+        <div className="text-4xl">Level {level.levelNo} Boss</div>
+        <div className="text-7xl">{level.position}</div>
+        <Link href={isUnlocked ? `level/${level.levelNo}` : '#'}><Button disabled={!isUnlocked}>Accept Offer</Button></Link>
+      </div>
+      {!isUnlocked && <div>Locked</div>}
     </div>
   );
 }
