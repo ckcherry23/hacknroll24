189821702de4c6d304d8382d0310003b5324611c
joeng03 <yinjoeng2003@gmail.com>
2024-01-21 02:19:30 +0800

    style: fix chatbox cancel button position

--- src/app/_components/bot/chatbox.tsx
+++ src/app/_components/bot/chatbox.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import React, { useEffect, useState } from 'react'
-import { LevelType, type ProfileType } from '@/lib/types'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import { CardHeader, Card, CardContent } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
-import image from "../../images/elonmusk.jpg"
-import Image from 'next/image';
+import React, { useEffect, useState } from "react";
+import { LevelType, type ProfileType } from "@/lib/types";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { CardHeader, Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import image from "../../images/elonmusk.jpg";
+import Image from "next/image";
 
 type ChatboxProps = {
   level: LevelType;
@@ -29,32 +29,36 @@ export default function Chatbox({ level, messages }: ChatboxProps) {
   }, [messages]);
 
   return (
-    <div className=''>
-      {
-        isOpen
-          ? <Card>
-            <CardHeader>
-              <div className='flex justify-between'>
-                <div className='flex flex-row gap-x-4'>
-                  <Avatar>
-                    {level.name == "Elon"
-                    ? <Image src={require("../../images/elonmusk.jpg")} alt="Elon Musk" width={50} height={50} />
-                    : (
-                      <>
+    <div className="">
+      {isOpen ? (
+        <Card>
+          <CardHeader>
+            <div className="flex justify-between">
+              <div className="flex flex-row gap-x-4">
+                <Avatar>
+                  {level.name == "Elon" ? (
+                    <Image
+                      src={require("../../images/elonmusk.jpg")}
+                      alt="Elon Musk"
+                      width={50}
+                      height={50}
+                    />
+                  ) : (
+                    <>
                       <AvatarImage src={level.imageUrl} alt="@shadcn" />
                       <AvatarFallback>{name}</AvatarFallback>
-                      </>
-                    )}
-                  </Avatar>
-                  <div>
-                    <div className='text-base font-bold'>{name}</div>
-                    <div className='text-base text-gray-200'>{position}</div>
-                  </div>
+                    </>
+                  )}
+                </Avatar>
+                <div>
+                  <div className="text-base font-bold">{name}</div>
+                  <div className="text-base text-gray-200">{position}</div>
                 </div>
               </div>
               <Button onClick={() => setIsOpen(false)} variant="secondary">
                 X
               </Button>
+            </div>
             {/* <div className='text-gray-200 italic font-bold uppercase'>Valuable feedback</div> */}
           </CardHeader>
           <CardContent>
@@ -76,7 +80,7 @@ export default function Chatbox({ level, messages }: ChatboxProps) {
             </div>
           </CardContent>
         </Card>
-       : (
+      ) : (
         // <Button
         //   className="rounded-full bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
         //   onClick={() => setIsOpen(true)}
