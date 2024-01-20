7f0a72ac1461c182315bc8e303438f76cef4c2f0
Charisma Kausar <charisma.kausar@gmail.com>
2024-01-21 01:55:06 +0800

    feat: add 404 page not found:

--- src/app/level/[id]/page.tsx
+++ src/app/level/[id]/page.tsx
@@ -1,11 +1,7 @@
-import { unstable_noStore as noStore } from "next/cache";
-import ChatBox from "@/app/_components/bot/chatbox";
-import Stage from "@/app/_components/stage";
-import { api } from "@/trpc/react";
 import { levels } from "@/levels"
 import { type LevelType } from "@/lib/types";
-import { useActiveCode } from '@codesandbox/sandpack-react'
 import Level from "@/app/_components/level";
+import ErrorNotFound from "@/app/_components/error-not-found";
 
 
 
@@ -17,8 +13,8 @@ export default async function LevelPage({ params }: { params: { id: string } }) 
   //   emotion_name: "Default",
   //   person_voice: "Elon Musk",
   // });
-  const level: LevelType = levels[parseInt(params.id)-1]!;
+  const level: LevelType = levels[parseInt(params.id) - 1]!;
   return (
-    <Level level={level} />
+    level ? <Level level={level} /> : <ErrorNotFound />
   )
 }
