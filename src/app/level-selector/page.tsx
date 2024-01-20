88ade1afc43957f2dc33cc93428805a634873fd7
Charisma Kausar <charisma.kausar@gmail.com>
2024-01-21 06:01:23 +0800

    style: switch bg image

--- src/app/level-selector/page.tsx
+++ src/app/level-selector/page.tsx
@@ -21,8 +21,8 @@ export default function LevelSelector() {
               return (<div key={index}><Image src={`/level-${level.levelNo}.png`} alt={level.name} width={200} height={200} /></div>);
             })}
           </div>
-          <div className="h-[10px] w-[3000px] bg-lime-900 opacity-100"></div>
-          <div className="h-[80px] w-[3000px] bg-stone-900 to-stone-900 opacity-100"></div>
+          <div className="h-[10px] w-[3000px] rounded-t-xl bg-lime-900 opacity-100"></div>
+          <div className="h-[80px] w-[3000px] rounded-b-xl bg-stone-900 to-stone-900 opacity-100"></div>
           <div className="flex items-center justify-center gap-8 overflow-hidden">
             {levels.map((level, index) => {
               return <LevelPreview key={index} level={level} />;
