a3afc6a6e32b484a9c5c2c83f92f537457d06bf8
tankh99 <khanghou99@gmail.com>
2024-01-20 21:46:29 +0800

    make level dynamic

--- src/app/_components/bot/chatbox.tsx
+++ src/app/_components/bot/chatbox.tsx
@@ -1,10 +1,8 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
-
 import React, { useEffect, useState } from 'react'
 import { LevelType, type ProfileType } from '@/lib/types'
-  import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { CardHeader, Card, CardContent } from '@/components/ui/card';
 
 type ChatboxProps = {
@@ -26,53 +24,6 @@ export default function Chatbox({ level, messages }: ChatboxProps) {
   useEffect(() => {
     setIsOpen(true)
   }, [messages])
-
-  return (
-    <div>
-      {
-        isOpen
-          ? <Card>
-            <CardHeader>
-              <div className='flex justify-between'>
-                <div className='flex flex-row gap-x-4'>
-                  <Avatar>
-                    <AvatarImage src={imageUrl} alt="@shadcn" />
-                    <AvatarFallback>{name}</AvatarFallback>
-                  </Avatar>
-                  <div>
-                    <div className='text-base font-bold'>{name}</div>
-                    <div className='text-base text-gray-200'>{position}</div>
-                  </div>
-                </div>
-                <Button onClick={() => setIsOpen(false)} variant='secondary'>X</Button>
-              </div>
-            </CardHeader>
-            <CardContent>
-              <div className='flex flex-col w-full'>
-                <div className='flex-grow'>
-                  {
-                    messages.map((message, index) => (
-                      <div key={index}>{message}</div>
-                    ))
-                  }
-                </div>
-                <div className='flex justify-between'>
-                  <input type="text" defaultValue='You are not allowed to send messages' className='w-full' disabled={true} />
-                  <Button disabled={true} variant='secondary'>Send</Button>
-                </div>
-              </div>
-            </CardContent>
-          </Card>
-          :
-          <div className='w-full flex justify-end'>
-            <Button
-              className='w-full'
-              onClick={() => setIsOpen(true)}
-              variant='secondary'
-            >Open Chatbox</Button>
-          </div>
-
-      }
-    </div>
-  )
 }
+
+  
