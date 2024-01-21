04c0a87c3777cb41a36dcfe522d5b6466e5868b5
tankh99 <khanghou99@gmail.com>
2024-01-21 12:01:17 +0800

    update delay, prompt

--- src/app/cutscene/two/messages.ts
+++ src/app/cutscene/two/messages.ts
@@ -11,24 +11,24 @@ export const messages: TelegramMessage[] = [
     message: "Yeah. Easiest way to get ahead is to make someone else do your work",
     sender: "Sarah",
     imageUrl: "/sarah.jpeg",
-    delay: 1
+    delay: 2
   },
   {
     message: "I should try that too next week!",
     sender: "Susan",
     imageUrl: "https://avatars.githubusercontent.com/u/5?v=5",
-    delay: 2
+    delay: 4
   },
   {
     message: "You think the intern'll still be at work in the weekends?",
     sender: "Susan",
     imageUrl: "https://avatars.githubusercontent.com/u/5?v=5",
-    delay: 3
+    delay: 6
   },
   {
     message: "You bet. That sucker is gonna be working overtime for me all week.",
     sender: "Sarah",
     imageUrl: "/sarah.jpeg",
-    delay: 4
+    delay: 8
   },
 ]
