091e5de6485b6518f842e359ed9e8e72507906e4
Charisma Kausar <charisma.kausar@gmail.com>
2024-01-21 09:21:46 +0800

    fix: add back previous level selector design

--- src/app/level-selector/level-preview.tsx
+++ src/app/level-selector/level-preview.tsx
@@ -1,58 +1,41 @@
-"use client";
+'use client';
 
 import { Button } from "@/components/ui/button";
 import { LevelType } from "@/lib/types";
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useRef } from "react";
-import { useLocalStorage } from "usehooks-ts";
+import { useLocalStorage } from 'usehooks-ts';
 
 type LevelPreviewProps = {
   level: LevelType;
-};
+}
 
 export default function LevelPreview({ level }: LevelPreviewProps) {
-  const [completedLevels] = useLocalStorage<Array<string>>("completedLevels", [
-    "0",
-  ]);
+  const [completedLevels] = useLocalStorage<Array<string>>(
+    'completedLevels',
+    ["0"],
+  );
   const ref = useRef<HTMLDivElement>(null);
 
-  const isUnlocked =
-    completedLevels.includes(level.levelNo) ||
-    parseInt(level.levelNo) ==
-      Math.max(...completedLevels.map((x) => parseInt(x))) + 1;
+  const isUnlocked = completedLevels.includes(level.levelNo)
+    || parseInt(level.levelNo) == Math.max(...completedLevels.map((x) => parseInt(x))) + 1;
 
   useEffect(() => {
     if (ref.current && isUnlocked) {
       ref.current.scrollIntoView({ behavior: "smooth", inline: "center" });
     }
-  }, [ref]);
+  }, [ref])
 
   return (
-    <div
-      className="relative h-[350px] border-r-[20px] border-stone-900"
-      ref={ref}
-    >
+    <div className="relative border-r-[20px] border-stone-900 h-[350px]" ref={ref}>
       <div>
-        {!isUnlocked && (
-          <div className="absolute left-6 top-32 z-10">
-            <Image
-              src={`/locked.png`}
-              alt="locked"
-              width={`50`}
-              height={`50`}
-            />
-          </div>
-        )}
+        {!isUnlocked && <div className="absolute z-10 top-32 left-6"><Image src={`/locked.png`} alt="locked" width={`100`} height={`100`} /></div>}
       </div>
-      <div
-        className={`flex flex-col gap-4 pt-12 ${!isUnlocked && "opacity-20"}`}
-      >
-        <div className="text-2xl">Level {level.levelNo} Boss</div>
-        <div className="text-4xl">{level.position}</div>
-        <Link href={isUnlocked ? `level/${level.levelNo}` : "#"}>
-          <Button disabled={!isUnlocked}>Accept Offer</Button>
-        </Link>
+      <div className={`w-[800px] flex flex-col gap-8 pt-12 pl-10 ${!isUnlocked && "opacity-20"}`}>
+        <div className="text-4xl">Level {level.levelNo} Boss</div>
+        <div className="text-7xl">{level.position}</div>
+        <Link href={isUnlocked ? `level/${level.levelNo}` : '#'}><Button disabled={!isUnlocked}>Start Day</Button></Link>
       </div>
     </div>
   );
--- src/app/level-selector/page.tsx
+++ src/app/level-selector/page.tsx
@@ -2,38 +2,36 @@
 
 import { levels } from "@/levels";
 import LevelPreview from "./level-preview";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function LevelSelector() {
+
   const resetProgress = () => {
-    window.localStorage.removeItem("completedLevels");
+    window.localStorage.removeItem('completedLevels');
     window.location.reload();
-  };
+  }
 
   return (
-    <div className="flex h-screen select-none items-center justify-center">
-      <Carousel>
-        <CarouselContent>
-          {levels.map((level, index) => {
-            return (
-              <CarouselItem key={index} className="basis-1/3">
-                <LevelPreview level={level} />
-              </CarouselItem>
-            );
-          })}
-          <CarouselItem key={levels.length} className="basis-1/3">
-            <div className="text-3xl">More levels coming soon!</div>
-          </CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
-    </div>
+    <main className="flex min-h-screen flex-col items-start justify-center bg-[url('/bg.jpeg')] bg-contain">
+      <div className="flex items-start justify-start">
+        <div className="hide-scroll-bar py-24 px-40">
+          <div className="flex space-x-[600px] ">
+            {levels.map((level, index) => {
+              return (<div key={index}><Image src={`/level-${level.levelNo}.png`} alt={level.name} width={200} height={200} /></div>);
+            })}
+          </div>
+          <div className="h-[10px] w-[3000px] rounded-t-xl bg-lime-900 opacity-100"></div>
+          <div className="h-[80px] w-[3000px] rounded-b-xl bg-stone-900 to-stone-900 opacity-100"></div>
+          <div className="flex items-center justify-center gap-8 overflow-hidden">
+            {levels.map((level, index) => {
+              return <LevelPreview key={index} level={level} />;
+            })}
+            <div className="p-4 text-2xl">More levels coming soon!</div>
+            <Button onClick={() => resetProgress()}>Reset Progress</Button>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
